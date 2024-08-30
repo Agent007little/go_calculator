30340ffff4b2d472286262ca5fb4ad4fcdbcf8d6
Agent007little <alekc_kg@mail.ru>
2024-08-30 18:46:23 +0300

    commit №3

--- calculator.go
+++ calculator.go
@@ -52,71 +52,69 @@ func toRoman(num int) string {
 	return roman
 }
 
+func checkError(condition bool, message string) {
+	if !condition {
+		panic(message)
+	}
+}
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Введите выражение (например: VI * IX):")
-	inp, _ := reader.ReadString('\n')
-	inp = strings.TrimSpace(inp)
 
-	parts := strings.Fields(inp)
-	if len(parts) != 3 {
-		fmt.Println("Ошибка: неверный ввод. Используйте формат 'число оператор число'.")
-		return
-	}
+	for {
+		fmt.Println("Введите выражение (например: VI * IX):")
+		inp, _ := reader.ReadString('\n')
+		inp = strings.TrimSpace(inp)
 
-	num1, sign, num2 := parts[0], parts[1], parts[2]
+		parts := strings.Fields(inp)
+		checkError(len(parts) == 3, "Ошибка: неверный ввод. Используйте формат 'число оператор число'.")
 
-	n1, ok1 := romeDict[num1]
-	n2, ok2 := romeDict[num2]
+		num1, sign, num2 := parts[0], parts[1], parts[2]
 
-	if ok1 && ok2 {
-		if n1 > 0 && n1 <= 10 && n2 > 0 && n2 <= 10 {
+		n1, ok1 := romeDict[num1]
+		n2, ok2 := romeDict[num2]
+
+		if ok1 && ok2 {
+			checkError(n1 > 0 && n1 <= 10 && n2 > 0 && n2 <= 10, "Ошибка: числа должны быть от I до X.")
+
 			switch sign {
 			case "+":
 				fmt.Println(toRoman(sum(n1, n2)))
 			case "-":
 				result := minus(n1, n2)
-				if result > 0 {
-					fmt.Println(toRoman(result))
-				} else {
-					fmt.Println("Ошибка: результат отрицательный или ноль.")
-				}
+				checkError(result > 0, "Ошибка: результат отрицательный или ноль.")
+				fmt.Println(toRoman(result))
 			case "/":
 				result := division(n1, n2)
-				if result >= 1 {
-					fmt.Println(toRoman(result))
-				} else {
-					fmt.Println("Ошибка: результат отрицательный или ноль.")
-				}
+				checkError(result >= 1, "Ошибка: результат отрицательный или ноль.")
+				fmt.Println(toRoman(result))
 			case "*":
 				fmt.Println(toRoman(mult(n1, n2)))
 			default:
-				fmt.Println("Ошибка: неверный оператор.")
+				panic("Ошибка: неверный оператор.")
 			}
-		} else {
-			fmt.Println("Ошибка: числа должны быть от I до X.")
-		}
-	} else if !ok1 && !ok2 {
-		num1, _ := strconv.Atoi(num1)
-		num2, _ := strconv.Atoi(num2)
+		} else if !ok1 && !ok2 {
+			num1Int, err1 := strconv.Atoi(num1)
+			num2Int, err2 := strconv.Atoi(num2)
 
-		if num1 > 0 && num1 <= 10 && num2 > 0 && num2 <= 10 {
+			checkError(err1 == nil && err2 == nil, "Ошибка: неверный ввод чисел.")
+			checkError(num1Int > 0 && num1Int <= 10 && num2Int > 0 && num2Int <= 10, "Ошибка: должны быть целые числа от 1 до 10.")
+
 			switch sign {
 			case "+":
-				fmt.Println(sum(num1, num2))
+				fmt.Println(sum(num1Int, num2Int))
 			case "-":
-				fmt.Println(minus(num1, num2))
+				fmt.Println(minus(num1Int, num2Int))
 			case "/":
-				fmt.Println(division(num1, num2))
+				checkError(num2Int != 0, "Ошибка: деление на ноль.")
+				fmt.Println(division(num1Int, num2Int))
 			case "*":
-				fmt.Println(mult(num1, num2))
+				fmt.Println(mult(num1Int, num2Int))
 			default:
-				fmt.Println("Ошибка: неверный оператор.")
+				panic("Ошибка: неверный оператор.")
 			}
 		} else {
-			fmt.Println("Ошибка: должны быть целые числа от 1 до 10.")
+			panic("Ошибка: неверный ввод чисел.")
 		}
-	} else {
-		fmt.Println("Ошибка: неверный ввод чисел.")
 	}
 }
